fd1d9ccde5efeb947086b122172fcb3689758751
Dan Journo <danjourno@gmail.com>
2024-12-19 17:15:07 +0000

    Implement Basket and Scan()

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -6,9 +6,11 @@ namespace Checkout
     public class Checkout : ICheckout
     {
         public List<Product> Products { get; }
+        List<BasketItem> basket = new List<BasketItem>();
 
         public Checkout(List<Product> products)
         {
+            //could use dependancy injection here instead to allow for data stored in SQL 
             Products = products;
         }
 
@@ -17,9 +19,20 @@ namespace Checkout
             throw new NotImplementedException();
         }
 
-        public void Scan(string item)
+        public void Scan(string SKU)
         {
-            throw new NotImplementedException();
+            var productFound = Products.FirstOrDefault(p => p.SKU == SKU);
+            if (productFound != null)
+            {
+                var productInBasket = basket.FirstOrDefault(b => b.SKU == SKU);
+                if (productInBasket != null)
+                    productInBasket.Quantity += 1;
+                else
+                    basket.Add(new BasketItem { SKU = SKU, Quantity = 1 });
+            } else
+            {
+                throw new ArgumentException($"Invalid item: {SKU}");
+            }
         }
     }
 }

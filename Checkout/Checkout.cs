7c4e8e1e2ff622ff193961854efde35b03fb4686
Dan Journo <danjourno@gmail.com>
2024-12-19 17:22:17 +0000

    Inital Implementation of GetTotalPrice

--- Checkout/Checkout.cs
+++ Checkout/Checkout.cs
@@ -5,23 +5,36 @@ namespace Checkout
 {
     public class Checkout : ICheckout
     {
-        public List<Product> Products { get; }
+        public List<Product> products { get; }
         List<BasketItem> basket = new List<BasketItem>();
 
         public Checkout(List<Product> products)
         {
             //could use dependancy injection here instead to allow for data stored in SQL 
-            Products = products;
+            this.products = products;
         }
 
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            int totalPrice = 0;
+
+            foreach(var basketItem in basket)
+            {
+                var product = products.First(p => p.SKU == basketItem.SKU);
+                if(product == null)
+                {
+                    throw new Exception($"Product not found with SKU {basketItem.SKU}");
+                }
+
+                totalPrice += (basketItem.Quantity * product.Price);
+            }
+
+            return totalPrice;
         }
 
         public void Scan(string SKU)
         {
-            var productFound = Products.FirstOrDefault(p => p.SKU == SKU);
+            var productFound = products.FirstOrDefault(p => p.SKU == SKU);
             if (productFound != null)
             {
                 var productInBasket = basket.FirstOrDefault(b => b.SKU == SKU);
--- Checkout/Classes/Product.cs
+++ Checkout/Classes/Product.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Checkout.Classes
 {
     public class Product
-    {
-        public string SKU { get; set; }
+    {        
+        public string SKU { get; set; } //depending on the volume of data and size of a basket, this could store a reference to the actual product, rather than the SKU code
         public int Price { get; set; }
         public ProductDiscount? Discount { get; set; }
     }
